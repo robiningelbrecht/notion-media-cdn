5ee8eb6c0f75a5bf90c302537a953bd25c1b7725
robiningelbrecht <robin@baldwin.be>
2022-09-21 12:40:44 +0200

    Structure for charts

--- api/index.php
+++ api/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Cache;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\AppFactory;
@@ -10,11 +9,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) {
-    $response->getBody()->write(Cache::forSvg()->get());
-    return $response->withHeader('Content-Type', 'image/svg+xml');
-});
-$app->get('/debug', function (ServerRequestInterface $request, ResponseInterface $response) {
-    $response->getBody()->write(Cache::forDebug()->get());
+    $response->getBody()->write('');
     return $response;
 });
 
