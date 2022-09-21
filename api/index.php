ef072ddb671613d3f84fcae541fabcd3e2027f77
robiningelbrecht <robin@baldwin.be>
2022-09-21 15:16:04 +0200

    Revert "Structure for charts"
    This reverts commit 5ee8eb6c0f75a5bf90c302537a953bd25c1b7725.

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Cache;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\AppFactory;
@@ -9,7 +10,11 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) {
-    $response->getBody()->write('');
+    $response->getBody()->write(Cache::forSvg()->get());
+    return $response->withHeader('Content-Type', 'image/svg+xml');
+});
+$app->get('/debug', function (ServerRequestInterface $request, ResponseInterface $response) {
+    $response->getBody()->write(Cache::forDebug()->get());
     return $response;
 });
 
